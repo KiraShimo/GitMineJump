5634114c50d04fc3dd570e593e58febbb22d4a02
Max <Max@DESKTOP-1KIJ07Q.Speedport_W_921V_1_44_000>
2018-05-31 19:27:38 +0200

    Hintergrund und Titelbild im Mainmenü hinzugefügt

--- MineJump/src/application/MainController.java
+++ MineJump/src/application/MainController.java
@@ -4,13 +4,12 @@ import application.ProgrammStartKlasse;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-public class MainController {
+public class MainController{
 	
 	public Main main;
 	
@@ -20,14 +19,17 @@ public class MainController {
 	
 	@FXML
 	private void BtnStartAction(ActionEvent actionevent) {
-		public void game(Stage primaryStage) {
+		/*@Override
+		public void start(Stage primaryStage) {
 			try {
-				Scene game = new Scene(game.getRootPane());
+				SpielFX Spiel = new SpielFX();
+				Scene game = new Scene(Spiel.getRootPane());
 		        primaryStage.setScene(game); 
 		        primaryStage.show();
 			} catch(Exception e) {
 				e.printStackTrace();
 			}
+		}*/
 	}
 	
 	@FXML
