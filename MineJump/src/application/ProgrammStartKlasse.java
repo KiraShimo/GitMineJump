c641988222a0025503d85a1e028fd8fe792c3864
Max Burkhardt <Max Burkhardt@DESKTOP-I16BTH6>
2018-05-31 10:31:53 +0200

    MINMAX

--- MineJump/src/application/ProgrammStartKlasse.java
+++ MineJump/src/application/ProgrammStartKlasse.java
@@ -4,54 +4,17 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import javafx.application.Application;
-import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 
 public abstract class ProgrammStartKlasse extends Application implements Runnable, KeyListener {
-
-	// public void init(Stage primaryStage) throws Exception {
-	// super.init();
-	// try {
-	// Scene SPIEL = new Scene(null, 700,700);
-	// primaryStage.setScene(SPIEL);
-	// primaryStage.show();
-	// setFocusable(true);
-	// }
-	// catch (Exception e) {
-	// e.printStackTrace();
-	// }
-	// }
 	
-	AnchorPane rootPane;
-
-	
-	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setResizable(false);
-
-		VBox SpielBox = new VBox(30);
-		SpielBox.setBackground(new Background(new BackgroundImage(new Image(""), null, null, null,
-				new BackgroundSize(45, 45, true, true, true, true))));
-	}
-
-	@Override
-	public void stop() throws Exception {
-		super.init();
-	}
-
-	public void exit() throws Exception {
-		super.init();
-	}
+	private AnchorPane rootPane;
 
 	@Override
 	public void run() {
 		while (true) {
-
+			//ein Kommentar 
 			// repaint();
 			// Hearbeat
 			try {
