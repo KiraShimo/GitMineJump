0225c00d1f8de1252624f7999c58695957bc3651
Max <Max@DESKTOP-1KIJ07Q.Speedport_W_921V_1_44_000>
2018-06-04 22:06:13 +0200

    MainController zurückgesetzt

--- MineJump/src/application/MainController.java
+++ MineJump/src/application/MainController.java
@@ -1,32 +1,22 @@
 package application;
 
+import application.ProgrammStartKlasse;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import application.ProgrammStartKlasse;
-
 public class MainController {
-	//=======
-	//>>>>>>>branch'master' of https:// github.com/KiraShimo/GitMineJump.git
-
+	
 	public Main main;
-
+	
 	public void setMain(Main main) {
 		this.main = main;
 	}
-
-	@FXML
-	private void BtnStartAction(ActionEvent actionevent){
-	//<<<<<<< HEAD
-
-	//=======
-		
+	
 	public void start(Stage primaryStage) {
-		//villeicht die start methode ausgliedern und dann aufrufen NICHT IN EINER ANDEREN METHODE SCHREIBEN
-		// >>>>>>> branch 'master' of https://github.com/KiraShimo/GitMineJump.git
+		
 		try {
 			ProgrammStartKlasse Spiel = new ProgrammStartKlasse();
 			Scene game = new Scene(Spiel.getRootPane());
@@ -35,14 +25,17 @@ public class MainController {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		// <<<<<<< HEAD
-		}
-	//>>>>>>>branch'master'of https:// github.com/KiraShimo/GitMineJump.git
+
+	}
+	
+	@FXML
+	private void BtnStartAction(ActionEvent actionEvent) {
+		
 	}
 	
 	@FXML
 	private void BtnExitAction(ActionEvent actionEvent) {
 		Platform.exit();
 	}
-
+	
 }
