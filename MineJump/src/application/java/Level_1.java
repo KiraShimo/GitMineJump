f3708fdfb222218a817664625cc018ab107c1c79
Max Burkhardt <Max Burkhardt@DESKTOP-I16BTH6>
2018-06-29 17:57:38 +0200

    ....

--- MineJump/src/application/java/Level_1.java
+++ MineJump/src/application/java/Level_1.java
@@ -303,7 +303,7 @@ public class Level_1 {
 		// Kollisionsdetektion *FUNKTIONIERT MEHR ODER WENIGER* *MACHT PROBLEME MIT
 		// SIEGBEDINGUNG* probier mal ein bisschen am ersten block im ersten lv
 		if (PosX <= B01PosX + 10)
-			if (PosX >= B01PosX && PosX <= B01PosX + 70 && PosY >= 240
+			if (PosX >= (B01PosX +70) && PosX <= (B01PosX + 70) && PosY >= 240
 					|| SPosXR >= B01PosX && SPosXR <= B01PosX + 70 && PosY >= 240) {
 				if (PosX <= B01PosX) {
 					PosX = B01PosX + 1;
