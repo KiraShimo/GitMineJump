cc6bed46785cd0f3ae5f576795aafd723974bcec
max.burkhardt <max.burkhardt@SHDEDRSW433.bechtle.net>
2018-08-15 10:45:48 +0200

    Importe gfixt

--- MineJump/src/application/java/MainController.java
+++ MineJump/src/application/java/MainController.java
@@ -1,9 +1,7 @@
 package application.java;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 
 import javafx.application.Platform;
