c99de4272586f4a845986e900a88ed6ad4e08d70
Max <Max@DESKTOP-1KIJ07Q>
2018-09-30 16:04:04 +0200

    bla

--- MineJump/src/application/java/Level_4.java
+++ MineJump/src/application/java/Level_4.java
@@ -5,6 +5,7 @@ import javafx.animation.AnimationTimer;
 import javafx.animation.KeyFrame;
 import javafx.animation.PauseTransition;
 import javafx.animation.Timeline;
+import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
